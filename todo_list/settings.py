cb5c03358d089f6c54d5c72d28ef705bbd247794
ekeukeni <ukenieke@gmail.com>
2022-11-02 17:13:30 +0100

    update settings

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -75,14 +75,23 @@ WSGI_APPLICATION = "todo_list.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "todo.sqlite3",
+#     }
+# }
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "todo.sqlite3",
+    'default': {
+        'NAME': 'tododb_5pbz',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'USER': 'ekeukeni',
+        'HOST':'dpg-cdh9arpgp3jpn5a2ngrg-a',
+        'PASSWORD': 'CHXERZfanVL28zg2hY6QuZHVeXc9x70o',
+        'PORT':5432
     }
 }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
